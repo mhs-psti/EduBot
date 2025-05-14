4941f826a796a0c86976cd492ee163b2458d808a
mhs-psti <mhs-psti@upi.edu>
2025-05-14 15:04:50 +0700

    Updated PdfViewerWeb.tsx

--- components/PdfViewerWeb.tsx
+++ components/PdfViewerWeb.tsx
@@ -1,27 +1,46 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as pdfjsLib from 'pdfjs-dist';
+
 pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
 
 export const PdfViewerWeb = ({ pdfUrl }: { pdfUrl: string }) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [numPages, setNumPages] = useState(0);
 
   useEffect(() => {
     const renderPDF = async () => {
       const loadingTask = pdfjsLib.getDocument(pdfUrl);
       const pdf = await loadingTask.promise;
-      const page = await pdf.getPage(1);
+      setNumPages(pdf.numPages);
 
-      const viewport = page.getViewport({ scale: 1.5 });
-      const canvas = canvasRef.current!;
-      const context = canvas.getContext('2d')!;
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
+      for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++) {
+        const page = await pdf.getPage(pageNumber);
+        const viewport = page.getViewport({ scale: 1.2 });
 
-      await page.render({ canvasContext: context, viewport }).promise;
+        const canvas = document.createElement('canvas');
+        canvas.style.marginBottom = '20px';
+        const context = canvas.getContext('2d')!;
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        await page.render({ canvasContext: context, viewport }).promise;
+
+        containerRef.current?.appendChild(canvas);
+      }
     };
 
     renderPDF();
   }, [pdfUrl]);
 
-  return <canvas ref={canvasRef} style={{ width: '100%' }} />;
+  return (
+    <div
+      ref={containerRef}
+      style={{
+        width: '100%',
+        height: '100%',
+        overflowY: 'auto',
+        padding: '16px',
+      }}
+    />
+  );
 };
