58aba1fc7bdc0c52e4e73031c298689666d82f6d
mhs-psti <mhs-psti@upi.edu>
2025-05-14 15:41:41 +0700

    Updated PdfViewer.web.tsx

--- components/pdf-viewer/PdfViewer.web.tsx
+++ components/pdf-viewer/PdfViewer.web.tsx
@@ -7,7 +7,6 @@ interface PdfViewerProps {
 }
 
 export const PdfViewer: React.FC<PdfViewerProps> = ({ uri, onError }) => {
-  console.log(uri);
   if (!uri) {
     return <View style={styles.container} />;
   }
