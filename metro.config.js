849d8138c392feab21b17a180db361f5f0771bc8
mhs-psti <mhs-psti@upi.edu>
2025-05-16 12:56:50 +0700

    Updated metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,13 @@
+// metro.config.js
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
