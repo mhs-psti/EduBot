e1c3847b3d6e07900a6e7a684f1f53020c611fbe
mhs-psti <mhs-psti@upi.edu>
2025-05-17 22:11:19 +0700

    Updated books.ts

---
+++ data/books.ts
@@ -0,0 +1,13 @@
+import { Book } from '../types/book';
+
+export const books: Book[] = [
+  {
+    id: '1',
+    title: 'Mathematics',
+    subject: 'Mathematics',
+    classLevel: 'VII',
+    description: 'Comprehensive mathematics textbook covering algebra, geometry, and arithmetic for 7th grade students.',
+    coverImage: 'https://images.pexels.com/photos/5088188/pexels-photo-5088188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    pdfUrl: 'https://chunkr-web.s3.us-east-1.amazonaws.com/landing_page/input/science.pdf'
+  }
+];
