63163f6ce00818c32af7c165f9fb306aa48e47c9
mhs-psti <mhs-psti@upi.edu>
2025-05-20 17:47:42 +0700

    Updated ReferenceTooltip.tsx

--- components/chat/ReferenceTooltip.tsx
+++ components/chat/ReferenceTooltip.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 export const ReferenceTooltip = ({ chunk }: { chunk: any }) => {
