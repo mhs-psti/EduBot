3499c81a624a118799ad8b33a7a5935af9202b1b
mhs-psti <mhs-psti@upi.edu>
2025-05-21 15:50:30 +0700

    Updated chat.ts

--- types/chat.ts
+++ types/chat.ts
@@ -1,14 +1,13 @@
-export interface ChatSession {
-  id: string;
-  bookId: string;
-  timestamp: number;
-  lastMessage: string;
-  messages: ChatMessage[];
-}
-
 export interface ChatMessage {
   id: string;
   content: string;
   timestamp: number;
   isUser: boolean;
+}
+
+export interface ChatSession {
+  id: string;
+  timestamp: number; // from update_time
+  lastMessage: string; // from last message if available
+  messages: ChatMessage[];
 }
